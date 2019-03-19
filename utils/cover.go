e03c7e265397e584e129658bb712103594917a92
fanghr <chfanghr@gmail.com>
2019-03-19 15:30:19 +0800

    make get cover image possible

--- utils/cover.go
+++ utils/cover.go
@@ -6,9 +6,18 @@ import (
 	"github.com/chfanghr/librespot/Spotify"
 )
 
-func GetCoverArtUrls(track *Spotify.Track) (res map[Spotify.Image_Size]string) {
+func GetTrackCoverArtUrls(track *Spotify.Track) (res map[Spotify.Image_Size]string) {
 	res = make(map[Spotify.Image_Size]string)
 	for _, v := range track.GetAlbum().GetCoverGroup().GetImage() {
+		url := fmt.Sprintf("https://i.scdn.co/image/%032s", hex.EncodeToString(v.GetFileId()))
+		res[v.GetSize()] = url
+	}
+	return
+}
+
+func GetAlbumCoverArtUrls(album *Spotify.Album) (res map[Spotify.Image_Size]string) {
+	res = make(map[Spotify.Image_Size]string)
+	for _, v := range album.GetCoverGroup().GetImage() {
 		url := fmt.Sprintf("https://i.scdn.co/image/%032s", hex.EncodeToString(v.GetFileId()))
 		res[v.GetSize()] = url
 	}
